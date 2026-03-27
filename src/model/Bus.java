f7b85146d4673d435043d1348d5d18c08f4f4ac7
kelvin john <kelvin12299@gmail.com>
2026-03-27 15:00:21 +0200

    add: my work

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -4,7 +4,33 @@ public class Bus extends Vehicle{
 	private  int numberOfseat ;
 	private  int BaggageDivision;
 	
+	
+	//getters
+	public int getnumberOfseat() {
+		return numberOfseat;
+	}
+	
+	public int  getBaggageDivision() {
+		return BaggageDivision;
+	}
+	
 	//setters
+	public void setnumberOfseat(int inputnumberOfseat) {
+		if (inputnumberOfseat > 0 && inputnumberOfseat < 25 ){
+		numberOfseat = inputnumberOfseat;
+		}
+		else {
+		numberOfseat = 7;
+		}
+ 	}
+	
+	public void setBaggageDivision(int inputBagg)
+
+	
+	
+
+	
+	
 	
 		
 	
