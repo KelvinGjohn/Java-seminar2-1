331539654df8d5717cd8cb89c8f8834e905261ec
kelvin john <kelvin12299@gmail.com>
2026-04-10 14:28:44 +0300

    added new file to purchase

--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Purchase {
+	private String userCarNo;
+	private ArrayList<Vehicle> shoppingList = new ArrayList<Vehicle>();
+	private LocalDateTime datetime = LocalDateTime.now();
+	
+	public String getUserCardNo() {
+		return userCarNo;
+	}
+	
+	public ArrayList<Vehincle>(){
+		return 
+	}
 
 }
